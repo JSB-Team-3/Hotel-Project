cd4f95bcc96404dcc4aa078674ee61bcc55f76d0
momensale7 <momensale2468@gmail.com>
2025-04-20 19:46:47 +0200

    refactor : using shared useToggle pass

--- src/modules/Authentication/ChangePass/ChangePasswordUtilities.ts
+++ src/modules/Authentication/ChangePass/ChangePasswordUtilities.ts
@@ -1,15 +1,4 @@
-import { useState } from 'react'
 
-function useTogglePassword() {
-    const [showPass, setShowPass] = useState<boolean>(false);
-    const [showConfirmPass, setConfirmPass] = useState<boolean>(false);
-    const [showOldPass, setOldPass] = useState<boolean>(false);
-    const handleShowPass = (): void => setShowPass((prev) => !prev);
-    const handleConfirmPass = (): void => setConfirmPass((prev) => !prev);
-    const handleOldPass = (): void => setOldPass((prev) => !prev);
-    return{showPass,showConfirmPass,showOldPass,handleShowPass,handleConfirmPass,handleOldPass}
-}
-export default useTogglePassword
 export const  maodalStyles = {
     position: 'absolute',
     top: '50%',
